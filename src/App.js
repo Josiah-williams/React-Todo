e2a042897a077c11e54c14d5004a80a715d24ecf
josiah <josiahdamiwilliams@gmail.com>
2019-12-03 15:20:50 +0100

    Refactored App.js to add state & given task data.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+// import ToDoList from './components/TodoComponents/TodoList'
+// import TodoForm from './components/TodoComponents/TodoForm'
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      taskData: [
+        {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+],
+// inputTask: '',
+// newTask: ''
+// };
+}
+}
   render() {
     return (
       <div>
@@ -12,5 +37,6 @@ class App extends React.Component {
     );
   }
 }
+
 
 export default App;
